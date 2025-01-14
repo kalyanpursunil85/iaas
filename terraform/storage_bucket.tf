e483d344eb4dd21ff18eb01e40eb2540a9d085d5
Sunil Kalyanpur <Sunil.Kalyanpur@viasat.com>
2025-01-13 19:19:03 -0800

    fix storage.tf

--- terraform/storage_bucket.tf
+++ terraform/storage_bucket.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "tf" {
   name          = "test-skalyanpur-bucket"
   location      = "US"
   force_destroy = true
-
+  uniform_bucket_level_access = true
   public_access_prevention = "enforced"
 }
 
@@ -10,5 +10,5 @@ resource "google_storage_bucket_object" "empty_folder" {
   name   = "empty_folder/" # folder name should end with '/'
   content = " "            # content is ignored but should be non-empty
   bucket = google_storage_bucket.tf.name
-  uniform_bucket_level_access = true
+
 }
